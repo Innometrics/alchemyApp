b7ec9010d688693d59495d3bdaa6f1014183f622
ernil <morion.ernil@gmail.com>
2015-10-28 13:13:17 +0300

    display real data

--- frontend/widgets/interests/index.js
+++ frontend/widgets/interests/index.js
@@ -9,7 +9,103 @@ $(function () {
      * Main entry point
      */
     inno.onReady(function () {
-        Loader.hide();
+        getInterestsData(function () {
+            Loader.hide();
+        });
     });
+
+    /**
+     * Get interests data collected by backend part of application
+     * @param callback
+     */
+    function getInterestsData (callback) {
+        inno.getProperties(function (status, data) {
+            var error = null,
+                interests = null;
+
+            if (!status) {
+                error = new Error('Can not get interests data');
+            } else {
+                data = data || {};
+                interests = data.commonData || {};
+            }
+
+            renderChart(interests);
+            callback(error, interests);
+        });
+    }
+
+    /**
+     * Render chart by certain data
+     * @param {Object} data
+     */
+    function renderChart (data) {
+        var plotData = prepareJQPlotData(data);
+        if (!plotData.length) {
+            $chart.html('No data for display');
+            return;
+        }
+
+        var config = getJQPlotConfigByType();
+
+        $.jqplot(
+            'chart',
+            [plotData],
+            config
+        );
+    }
+
+    /**
+     * Prepare data for JQPlot config
+     * @param interests
+     */
+    function prepareJQPlotData (interests) {
+        return Object.keys(interests).map(function (name) {
+            return [
+                name,
+                interests[name]
+            ];
+        }).sort(function (rec1, rec2) {
+            // sort by values
+            return rec2[1] - rec1[1];
+        });
+    }
+
+
+    /**
+     * returns config for JQPlot
+     * @return {Object} Config
+     */
+    function getJQPlotConfigByType () {
+        return {
+            grid: {
+                borderWidth: 0,
+                shadow: false
+            },
+            axesDefaults: {
+                tickRenderer: $.jqplot.CanvasAxisTickRenderer,
+                tickOptions: {
+                    angle: -15,
+                    fontSize: '10pt'
+                }
+            },
+            axes: {
+                xaxis: {
+                    renderer: $.jqplot.CategoryAxisRenderer
+                }
+            },
+            series: [{
+                renderer: $.jqplot.BarRenderer,
+                rendererOptions: {
+                    shadowOffset: 0
+                }
+            }],
+            highlighter: {
+                show: true,
+                sizeAdjust: 7.5,
+                tooltipAxes: 'y'
+            }
+        };
+    }
 
 });
