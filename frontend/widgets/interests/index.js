a68e80d196fd318fe9c776772f06e68a1e0683ec
ernil <morion.ernil@gmail.com>
2015-10-28 16:14:54 +0300

    configure widget using settings

--- frontend/widgets/interests/index.js
+++ frontend/widgets/interests/index.js
@@ -15,6 +15,20 @@ $(function () {
     });
 
     /**
+     * Get widget settings about which interests to show on chart
+     * @param callback
+     */
+    function getWidgetSettings (callback) {
+        inno.getWidgetSettings(function (status, data) {
+            var error = null;
+            if (!status) {
+                error = new Error('Can not get settings');
+            }
+            callback(error,  data);
+        });
+    }
+
+    /**
      * Get interests data collected by backend part of application
      * @param callback
      */
@@ -30,8 +44,25 @@ $(function () {
                 interests = data.commonData || {};
             }
 
-            renderChart(interests);
-            callback(error, interests);
+            getWidgetSettings(function (error, settings) {
+                if (error) {
+                    console.error(error);
+                } else {
+
+                    settings = settings || {};
+                    var interest,
+                        interestsToShow = settings.showInterests ? settings.showInterests : [];
+
+                    for (interest in interests) {
+                        if (interestsToShow.indexOf(interest) === -1) {
+                            delete interests[interest];
+                        }
+                    }
+
+                    renderChart(interests, settings);
+                }
+                callback(error, interests);
+            });
         });
     }
 
@@ -39,14 +70,14 @@ $(function () {
      * Render chart by certain data
      * @param {Object} data
      */
-    function renderChart (data) {
+    function renderChart (data, settings) {
         var plotData = prepareJQPlotData(data);
         if (!plotData.length) {
             $chart.html('No data for display');
             return;
         }
 
-        var config = getJQPlotConfigByType();
+        var config = getJQPlotConfigByType(settings.chartType);
 
         $.jqplot(
             'chart',
@@ -76,36 +107,77 @@ $(function () {
      * returns config for JQPlot
      * @return {Object} Config
      */
-    function getJQPlotConfigByType () {
-        return {
-            grid: {
-                borderWidth: 0,
-                shadow: false
-            },
-            axesDefaults: {
-                tickRenderer: $.jqplot.CanvasAxisTickRenderer,
-                tickOptions: {
-                    angle: -15,
-                    fontSize: '10pt'
-                }
-            },
-            axes: {
-                xaxis: {
-                    renderer: $.jqplot.CategoryAxisRenderer
-                }
-            },
-            series: [{
-                renderer: $.jqplot.BarRenderer,
-                rendererOptions: {
-                    shadowOffset: 0
-                }
-            }],
-            highlighter: {
-                show: true,
-                sizeAdjust: 7.5,
-                tooltipAxes: 'y'
-            }
-        };
+    function getJQPlotConfigByType (type) {
+        var config;
+        type = type || 'bar';
+
+        switch (type) {
+
+            case 'pie':
+                config = {
+                    grid: {
+                        borderWidth: 0,
+                        shadow: false
+                    },
+                    seriesDefaults: {
+                        renderer: $.jqplot.PieRenderer,
+                        rendererOptions: {
+                            showDataLabels: true,
+                            padding: 0,
+                            shadowOffset: 0
+                        }
+                    },
+                    legend: {
+                        show:true,
+                        location: 'e',
+                        escapeHtml: true
+                    },
+                    highlighter: {
+                        show: true,
+                        formatString:'%s',
+                        tooltipLocation:'n',
+                        useAxesFormatters:false
+                    }
+                };
+                break;
+
+            case 'bar':
+                config = {
+                    grid: {
+                        borderWidth: 0,
+                        shadow: false
+                    },
+                    axesDefaults: {
+                        tickRenderer: $.jqplot.CanvasAxisTickRenderer,
+                        tickOptions: {
+                            angle: -15,
+                            fontSize: '10pt'
+                        }
+                    },
+                    axes: {
+                        xaxis: {
+                            renderer: $.jqplot.CategoryAxisRenderer
+                        }
+                    },
+                    series: [{
+                        renderer: $.jqplot.BarRenderer,
+                        rendererOptions: {
+                            shadowOffset: 0
+                        }
+                    }],
+                    highlighter: {
+                        show: true,
+                        sizeAdjust: 7.5,
+                        tooltipAxes: 'y'
+                    }
+                };
+                break;
+
+            default:
+                throw new Error('Unsupported chart type: ' + type);
+        }
+
+        return config;
     }
 
 });
